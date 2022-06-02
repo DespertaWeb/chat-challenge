faeedbd8fecb13766b3a559df46b781be8728652
Lou Rectoret <despertaweb@protonmail.com>
2022-06-02 18:45:15 +0200

    New - Display the name and photo of the user, as well as the text of the most recent message

--- src/views/LeftSidebar.vue
+++ src/views/LeftSidebar.vue
@@ -1,14 +1,16 @@
 <template>
   <section>
-    <p v-for="(contact, index) in startedChats" :key="index">
-      <h3>{{ new Date(contact.getLastChat().date).toLocaleDateString() }}</h3>
-      {{ contact.getLastChat().text }}
-    </p>
+    <ContactCard
+      v-for="(contact, index) in startedChats"
+      :key="index"
+      :contact="contact"
+    />
   </section>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
+import ContactCard from '../components/left-side-bar/ContactCard.vue'
 import { contacts as Contacts } from '../services/serializeData'
 
 const contacts = ref(Contacts)
@@ -20,8 +22,4 @@ const startedChats = computed(() => {
 })
 </script>
 
-<style lang="sass" scoped>
-p
-  padding: 7px 10px
-  border-bottom: var(--border)
-</style>
+<style lang="sass" scoped></style>
