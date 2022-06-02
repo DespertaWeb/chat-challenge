8d97a2c804a3b0f3f876afcb4366fba6e5c7c6a1
Lou Rectoret <despertaweb@protonmail.com>
2022-06-02 20:19:58 +0200

    WIP - Filter by name ok but not including non contacted contacts

---
+++ src/components/left-side-bar/ContactCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <li>
+    <img alt="Contact" class="avatar" :src="`/src/assets/${contact.photo}`" />
+    <article>
+      <header>
+        <h3 class="green">{{ contact.contactName }}</h3>
+        <span v-if="lastChat !== undefined"> {{ lastDate }} </span>
+      </header>
+      <p v-if="lastChat !== undefined">
+        {{ lastChat }}
+      </p>
+    </article>
+  </li>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  contact: Object
+})
+
+const lastChat = computed(() => {
+  return props.contact.getLastChat().text
+})
+const lastDate = computed(() => {
+  return new Date(props.contact.getLastChat().date).toLocaleDateString()
+})
+</script>
+
+<style lang="sass" scoped>
+li
+  display: flex
+  border-bottom: var(--border)
+  height: 5rem
+
+  .avatar
+    height: 5rem
+    width: 5rem
+
+  article
+    overflow: hidden
+    padding: var(--padding-sm)
+    width: 100%
+    display: block
+    height: inherit
+
+    header
+      grid-template-columns: 1fr auto
+      display: grid
+
+      h3
+        overflow: hidden
+        height: 1.6rem
+
+      span
+        margin-left: 0.7rem
+        color: var(--vt-c-text-light-2)
+
+    p
+      overflow: hidden
+      color: var(--vt-c-text-light-2)
+      max-height: 2.2rem
+      line-height: 1.1
+</style>
--- src/views/LeftSidebar.vue
+++ src/views/LeftSidebar.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
+    <SearchInput v-model="search" />
     <ContactCard
-      v-for="(contact, index) in startedChats"
+      v-for="(contact, index) in listedChats"
       :key="index"
       :contact="contact"
     />
@@ -11,14 +12,31 @@
 <script setup>
 import { ref, computed } from 'vue'
 import ContactCard from '../components/left-side-bar/ContactCard.vue'
+import SearchInput from '../components/left-side-bar/SearchInput.vue'
 import { contacts as Contacts } from '../services/serializeData'
 
 const contacts = ref(Contacts)
+const search = ref('')
 
 const startedChats = computed(() => {
   return contacts.value.filter(contact => {
     return contact.chats.length > 0
   })
+})
+
+const listedChats = computed(() => {
+  if (search.value !== '' && search.value !== null) {
+    return startedChats.value.filter(contact => {
+      return (
+        contact.contactName.toLowerCase().search(search.value.toLowerCase()) !==
+        -1
+      )
+    })
+  }
+  console.log('contacts.value', contacts.value.length)
+
+  return contacts.value
+  // search.value
 })
 </script>
 
