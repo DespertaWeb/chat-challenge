14dce07262b4d364e8f8ff8028d6d1138c4f66a6
Lou Rectoret <despertaweb@protonmail.com>
2022-06-02 17:06:03 +0200

    New - Service: to serialize raw data

---
+++ src/components/left-side-bar/ContactCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <li>
+    <img alt="Contact" class="avatar" :src="`/src/assets/${contact.photo}`" />
+    <article>
+      <h3 class="green">{{ contact.contactName }}</h3>
+      <p>
+        {{ lastChat }}
+      </p>
+    </article>
+  </li>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  contact: Object
+})
+
+const lastChat = computed(() => {
+  return props.contact.chats[props.contact.chats.length - 1].text
+})
+</script>
+
+<style lang="sass" scoped>
+li
+  display: flex
+  border-bottom: var(--border)
+  height: 5rem
+
+  .avatar
+    height: 5rem
+    width: 5rem
+
+  article
+    overflow: hidden
+    padding: var(--padding-sm)
+    width: auto
+    height: inherit
+
+    h3
+      display: block
+      overflow: hidden
+
+    p
+      overflow: hidden
+      color: var(--vt-c-text-light-2)
+      max-height: 2.2rem
+      line-height: 1.1
+</style>
---
+++ src/services/serializeData.js
@@ -0,0 +1,55 @@
+import CONTACTS from '../assets/mockdata/chatsSet.json'
+import RECEIVED_MSGS from '../assets/mockdata/ReceivedMessagesSet.json'
+import SENT_MSGS from '../assets/mockdata/SentMessagesSet.json'
+const RAW_CHATS = [...RECEIVED_MSGS,...SENT_MSGS]
+
+export const contacts = []
+
+/**
+ * Contact Class
+ * @param phone
+ * @param contactName
+ * @param photo
+ * @param chats
+ * If chat's empty => The contact hasn't been contacted but still should be able to start a chat with
+ **/
+ class Contact {
+  constructor(phone, contactName, photo, chats) {
+    this.phone = phone,
+    this.contactName = contactName,
+    this.photo = photo
+    this.chats = chats
+  }
+}
+
+/**
+ * Group msgs by chat
+ * No time but should be async
+ **/
+const conversations = RAW_CHATS.reduce((r, a) => {
+  let rawDate = a.date.slice(0, 10)
+  rawDate = rawDate.split('/').reverse().join().replaceAll(',', '-')
+  const rawTime = a.date.slice(10)+':00Z'
+
+  a.date = new Date(rawDate+rawTime)
+  r[a.phone] = [...r[a.phone] || [], a]
+  return r
+}, {})
+
+
+/**
+ * SORT Conversations
+ * No time but should be async
+ **/
+Object.values(conversations).forEach((unSortedChats, i) => {
+  const sortedChats = [...unSortedChats.sort((a, b) => a.date - b.date)]
+})
+
+
+/**
+ * Create contacts serlialized list
+ * No time to, this but should be happening right after grouping by chat and sorting the chat itself
+ */
+CONTACTS.forEach(contact => {
+  contacts.push(new Contact(contact.phone, contact.contactName, contact.photo, conversations[contact.phone] || []))
+})
