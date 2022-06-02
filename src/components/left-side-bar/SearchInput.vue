e99c295c04be17fbd371f2af39fc642a008b9049
Lou Rectoret <despertaweb@protonmail.com>
2022-06-02 20:20:17 +0200

    WIP - Filter by name ok but not including non contacted contacts

---
+++ src/components/left-side-bar/SearchInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <label for="searchInput">
+    <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </label>
+</template>
+
+<script setup>
+defineProps(['modelValue'])
+defineEmits(['update:modelValue'])
+</script>
+
+<style lang="sass" scoped>
+label
+  display: flex
+  align-items: center
+  border-bottom: var(--border)
+  height: 5rem
+  &::after
+    content: ''
+    display: block
+    background: url('@/assets/icons/search.svg')
+    width: 24px
+    height: 24px
+    position: absolute
+    right: 2rem
+
+  input
+    padding: var(--padding-sm) var(--padding-md)
+    margin: var(--padding-md)
+    margin-right: var(--padding-xl)
+    width: 100%
+</style>
